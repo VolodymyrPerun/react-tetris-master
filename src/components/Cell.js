17f6a3a85cf079fbf30aea6b3312008092e267a0
VolodymyrPerun <47578369+VolodymyrPerun@users.noreply.github.com>
2019-08-19 01:35:24 +0300

    innit

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <div>cell</div>
+    <StyledCell type={'L'} color={TETROMINOS['L'].color}>cell</StyledCell>
 )
 
 export default Cell;
