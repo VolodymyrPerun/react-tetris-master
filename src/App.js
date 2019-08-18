399550395e4c791bbe3a04fa7d976948af53a681
VolodymyrPerun <47578369+VolodymyrPerun@users.noreply.github.com>
2019-08-19 00:30:34 +0300

    innit
    new reast tetris project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Tetris from './components/Tetris';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+  <Tetris />
+  </div>
+);
+
+export default App;
