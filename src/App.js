e84088530ba8b5fc4948ecb1fa41162c30318951
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-11-24 19:41:35 +0200

    finished app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Tetris from './components/Tetris';
-import './App.css';
 
 const App = () => (
   <div className="App">
-  <Tetris />
+    <Tetris />
   </div>
 );
 
